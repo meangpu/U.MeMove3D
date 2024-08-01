cab9cc7491fa0668447b675bca3099b2f0320839
meangpu <51825872+meangpu@users.noreply.github.com>
2024-08-02 00:32:15 +0700

    update name space

--- Runtime/FirstPerson/FPSMovement.cs
+++ Runtime/FirstPerson/FPSMovement.cs
@@ -1,100 +1,104 @@
 using UnityEngine;
 
-public class FPSMovement : MonoBehaviour
+
+namespace Meangpu.Move3D.FPS
 {
-    [Header("Movement")]
-    [SerializeField] float moveSpeed;
-
-    [SerializeField] float groundDrag;
-
-    [SerializeField] float jumpForce;
-    [SerializeField] float jumpCooldown;
-    [SerializeField] float airMultiplier;
-    bool readyToJump;
-
-    [Header("KeyBinds")]
-    [SerializeField] KeyCode jumpKey = KeyCode.Space;
-
-    [Header("Ground Check")]
-    [SerializeField] float playerHeight;
-    [SerializeField] LayerMask whatIsGround;
-    bool grounded;
-
-    [SerializeField] Transform orientation;
-
-    float horizontalInput;
-    float verticalInput;
-
-    Vector3 moveDirection;
-
-    Rigidbody rb;
-
-    private void Start()
+    public class FPSMovement : MonoBehaviour
     {
-        rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true;
-        readyToJump = true;
-    }
+        [Header("Movement")]
+        [SerializeField] float moveSpeed;
 
-    private void Update()
-    {
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.3f, whatIsGround);
+        [SerializeField] float groundDrag;
 
-        GetInput();
-        ControlSpeed();
+        [SerializeField] float jumpForce;
+        [SerializeField] float jumpCooldown;
+        [SerializeField] float airMultiplier;
+        bool readyToJump;
 
-        if (grounded)
-            rb.linearDamping = groundDrag;
-        else
-            rb.linearDamping = 0;
-    }
+        [Header("KeyBinds")]
+        [SerializeField] KeyCode jumpKey = KeyCode.Space;
 
-    private void FixedUpdate()
-    {
-        MovePlayer();
-    }
+        [Header("Ground Check")]
+        [SerializeField] float playerHeight;
+        [SerializeField] LayerMask whatIsGround;
+        bool grounded;
 
-    private void GetInput()
-    {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticalInput = Input.GetAxisRaw("Vertical");
+        [SerializeField] Transform orientation;
 
-        if (Input.GetKey(jumpKey) && readyToJump && grounded)
+        float horizontalInput;
+        float verticalInput;
+
+        Vector3 moveDirection;
+
+        Rigidbody rb;
+
+        private void Start()
         {
-            readyToJump = false;
-            Jump();
-            Invoke(nameof(ResetJump), jumpCooldown);
+            rb = GetComponent<Rigidbody>();
+            rb.freezeRotation = true;
+            readyToJump = true;
         }
-    }
 
-    private void MovePlayer()
-    {
-        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
-
-        if (grounded)
-            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
-
-        else if (!grounded)
-            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
-    }
-
-    private void ControlSpeed()
-    {
-        Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
-
-        // limit velocity
-        if (flatVel.magnitude > moveSpeed)
+        private void Update()
         {
-            Vector3 limitedVel = flatVel.normalized * moveSpeed;
-            rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+            grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.3f, whatIsGround);
+
+            GetInput();
+            ControlSpeed();
+
+            if (grounded)
+                rb.linearDamping = groundDrag;
+            else
+                rb.linearDamping = 0;
         }
-    }
 
-    private void Jump()
-    {
-        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
-        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
-    }
+        private void FixedUpdate()
+        {
+            MovePlayer();
+        }
 
-    private void ResetJump() => readyToJump = true;
+        private void GetInput()
+        {
+            horizontalInput = Input.GetAxisRaw("Horizontal");
+            verticalInput = Input.GetAxisRaw("Vertical");
+
+            if (Input.GetKey(jumpKey) && readyToJump && grounded)
+            {
+                readyToJump = false;
+                Jump();
+                Invoke(nameof(ResetJump), jumpCooldown);
+            }
+        }
+
+        private void MovePlayer()
+        {
+            moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+            if (grounded)
+                rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+
+            else if (!grounded)
+                rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+        }
+
+        private void ControlSpeed()
+        {
+            Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+            // limit velocity
+            if (flatVel.magnitude > moveSpeed)
+            {
+                Vector3 limitedVel = flatVel.normalized * moveSpeed;
+                rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+            }
+        }
+
+        private void Jump()
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+        }
+
+        private void ResetJump() => readyToJump = true;
+    }
 }
---
+++ Runtime/PlayerMovementInput.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Meangpu.Move3D
+{
+    public class PlayerMovementInput : MonoBehaviour
+    {
+        [SerializeField] InputActionReference _moveInputAction;
+        [SerializeField] Vector3Reference _moveOutput;
+        Vector3 _moveDirection = new();
+        Vector2 _moveInput;
+
+        void OnEnable() => _moveInputAction.action.Enable();
+        void OnDisable() => _moveInputAction.action.Disable();
+
+        public Vector3 GetMovementVectorNormalize()
+        {
+            _moveInput = _moveInputAction.action.ReadValue<Vector2>();
+            _moveDirection.Set(_moveInput.x, 0, _moveInput.y);
+            return _moveDirection.normalized;
+        }
+        private void Update() => _moveOutput.Variable.SetValue(GetMovementVectorNormalize());
+    }
+}
